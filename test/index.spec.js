2cdac25906286e5f87d84597f3a7e35db0e2fe5b
Oliver Findl <github@findl.sk>
2020-01-28 19:37:58 +0100

    Updated specification of tests to avoid error from babel-plugin-tester

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const { resolve } = require("path");
-const pluginTester = require("babel-plugin-tester/pure").default;
+const { default: pluginTester } = require("babel-plugin-tester/pure");
 
+const { name: pluginName, version: pluginVersion } = require(resolve(__dirname, "../package.json"));
 const pluginPath = resolve(__dirname, "../src");
 const fixturesPath = resolve(__dirname, "fixtures");
 
 pluginTester({
 	plugin: require(pluginPath),
+	pluginName: `${pluginName}@${pluginVersion}`,
 	pluginOptions: { verbose: false },
 	fixtures: fixturesPath
 });
